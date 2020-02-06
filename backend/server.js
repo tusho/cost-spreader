f97f08d706ff54161870f9c9c5e0edc2f38af703
Armin Kuhestani <armin.kuhestani@gmail.com>
2020-02-06 16:15:06 +1100

    Added mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,8 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import logger from 'morgan';
-import mongoose from 'mongoose';
-
-const app = express();
-const router = express.Router();
-
+import { getSecret } from './secrets';
 const API_PORT = process.env.API_PORT || 3001;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(logger('dev'));
-
-router.get('/', (req, res) => {
-  res.json({ message: 'Welcome to Cost Spreader!' });
-});
-
-app.use('/api', router);
-
-app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
+// db config -- set your URI from mLab in secrets.js
+var mongoose = require('mongoose');
+mongoose.connect(getSecret('dbUri'));
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));

d49445cbbedf42f0f3e28da773f53e43a0597dc4
Armin Kuhestani <armin.kuhestani@gmail.com>
2020-02-07 11:12:06 +1100

    Front-end now fetching data from mongoDB

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,23 +1,57 @@
 import React, { Component } from 'react';
-import data from './data';
+import 'whatwg-fetch';
 import ProductList from './ProductList';
+import Product from './Product';
 import './Dashboard.css';
 
-class CommentBox extends Component {
+class Dashboard extends Component {
   constructor() {
     super();
-    this.state = { data: [] };
+    this.state = {
+      data: [],
+      error: null,
+      user: '',
+      product: '',
+      detail: ''
+    };
+    this.pollInterval = null;
   }
+
+  componentDidMount() {
+    this.loadProductsFromServer();
+    // if (!this.pollInterval) {
+    //   this.pollInterval = setInterval(this.loadProductsFromServer, 2000);
+    // }
+  }
+
+  // componentWillUnmount() {
+  //   if (this.pollInterval) clearInterval(this.pollInterval);
+  //   this.pollInterval = null;
+  // }
+
+  loadProductsFromServer = () => {
+    fetch('/api/products/')
+      .then(data => data.json())
+      .then((res) => {
+        if (!res.success) this.setState({ error: res.error });
+        else this.setState({ data: res.data });
+      });
+  }
+
   render() {
     return (
       <div className="container">
         <div className="products">
           <h2>Products:</h2>
-          <ProductList data={data} />
+          <ProductList data={this.state.data} />
         </div>
+      <div className="form">
+        <Product user={this.state.user} product={this.state.product} detail={this.state.detail} />
       </div>
+      {this.state.error && <p>{this.state.error}</p>}
+    </div>
     );
   }
 }
 
-export default CommentBox;
+export default Dashboard;

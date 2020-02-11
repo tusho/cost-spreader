439ebaca0b0536cf6f28f7d2a88808b6fa7eb805
Armin Kuhestani <armin.kuhestani@gmail.com>
2020-02-11 15:47:52 +1100

    Quick variable update

--- backend/models/item.js
+++ backend/models/item.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const ItemsSchema = new Schema({
   product: String,
   detail: String,
-}, { boughtOn: Date });
+}, { timestamps: true });
 
 // export our module to use in server.js
 export default mongoose.model('Item', ItemsSchema);
